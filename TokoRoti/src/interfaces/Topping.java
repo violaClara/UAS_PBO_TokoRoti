3dc8c1e9b6bef8db0227c74e0c2142da13736700
MuhAgungN <89763501+MuhAgungN@users.noreply.github.com>
2022-12-31 15:25:29 +0700

    Add files via upload

---
+++ TokoRoti/src/interfaces/Topping.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package interfaces;
+
+/**
+ *
+ * @author DELL
+ */
+public interface Topping {
+    public void hitungBeratTopping();
+    public double hitungHargaTopping();
+}
