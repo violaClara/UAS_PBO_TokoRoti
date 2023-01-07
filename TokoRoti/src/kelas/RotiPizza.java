61a3b6ea4d071a177d9a46057384d94a1f4a5495
ferdhiansurya <89855506+ferdhiansurya@users.noreply.github.com>
2023-01-07 10:08:29 +0700

    Add files via upload

--- TokoRoti/src/kelas/RotiPizza.java
+++ TokoRoti/src/kelas/RotiPizza.java
@@ -5,13 +5,25 @@
  */
 package tokoroti;
 
+import interfaces.Topping;
+
 /**
  *
  * @author acer
  */
-public class RotiPizza extends abstraksi.Adonan{
+public class RotiPizza extends abstraksi.Adonan implements Topping{
 
-
+    int beratKejuTopping=30;
+    double beratKejuToppingPSN;
+    
+    int beratSosisTopping=50;
+    double beratSosisToppingPSN;
+    
+    int beratSmokedBeefTopping=50;
+    double beratSmokedBeefToppingPSN;
+    
+    int beratBawangBombayTopping=30;
+    double beratBawangBombayToppingPSN;
     
 
     @Override
@@ -34,8 +46,42 @@ public class RotiPizza extends abstraksi.Adonan{
         System.out.println("Berat Susu Cair: "+beratSusuCairPSN+" gr");
         System.out.println("Berat Telur: "+beratTelurPSN+" gr");
         System.out.println("Berat Es Batu: "+beratEsBatuPSN+" gr"); 
+    }
+    
+            @Override
+    public void hitungBeratTopping() {
+        this.beratKejuToppingPSN=this.beratKejuTopping*jumlahPesanan;
+        this.beratSosisToppingPSN=this.beratSosisTopping*jumlahPesanan;
+        this.beratSmokedBeefToppingPSN=this.beratSmokedBeefTopping*jumlahPesanan;
+        this.beratBawangBombayToppingPSN=this.beratBawangBombayTopping*jumlahPesanan;
         
-       
+        System.out.println("Berat Topping Keju: "+beratKejuToppingPSN+" gr");
+        System.out.println("Berat Topping Sosis: "+beratSosisToppingPSN+" gr");
+        System.out.println("Berat Topping Smoked Beef: "+beratSmokedBeefToppingPSN+" gr");
+        System.out.println("Berat Topping Bawang Bombay: "+beratBawangBombayToppingPSN+" gr");
+    }
+
+    @Override
+    public double hitungHargaTopping() {
+        double hargaKejuTopping=(31000/250)*beratKejuToppingPSN;    
+        double hargaSosisTopping=(80000/1000)*beratSosisToppingPSN;    
+        double hargaSmokedBeefTopping=(90000/1000)*beratSmokedBeefToppingPSN;    
+        double hargaBawangBombayTopping=(40000/500)*beratBawangBombayToppingPSN;    
+        double hargaTopping=hargaKejuTopping+hargaSosisTopping+hargaSmokedBeefTopping+hargaBawangBombayTopping;
+        return hargaTopping;
+    }
+    
+     public void tampilkan(){
+        System.out.println("Roti Pizza Varian");
+        System.out.println("Jumlah: "+jumlahPesanan+" pcs \n");
+        hitungBeratBahan();
+        hitungBeratTopping();
+        System.out.println("-------------------");
+        System.out.println("Harga Bahan: Rp"+hitungHargaBahan());
+        System.out.println("Harga Topping: Rp"+hitungHargaTopping());
         
+        double hargaJual= hitungHargaBahan()+hitungHargaTopping()+((hitungHargaBahan()+hitungHargaTopping())*(10/100));
+        System.out.println("Harga Jual Per pcs: Rp"+hargaJual/jumlahPesanan);
+        System.out.println("\n \n");
     }
 }
